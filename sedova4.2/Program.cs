06069ef8fd5e0786f4b2b320ffade4f159e50fc7
Анна <@DASHA>
2021-11-21 20:02:40 +0200

    Добавьте файлы проекта.

---
+++ sedova4.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace sedova4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mas = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    mas[i, j] = 0;
+                }
+            }
+            int n = 5 / 2;
+            mas[n,n] = 1;
+            mas[n, n - 1] = 1;
+            mas[n, n - 2] = 1;
+            mas[n, n + 1] = 1;
+            mas[n, n + 2] = 1;
+            mas[n + 1, n] = 1;
+            mas[n + 2, n] = 1;
+            mas[n - 1, n] = 1;
+            mas[n - 2, n] = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(mas[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
